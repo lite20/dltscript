5ac868e7022f378f34aae8be59e3cb30f4f308fa
lite20 <lite260@gmail.com>
2018-10-30 21:40:36 -0600

    parsing

--- include/parser.hpp
+++ include/parser.hpp
@@ -2,11 +2,13 @@
 
 #include <vector>
 
+#include "tokenizer.hpp"
+
 namespace
 {
     enum node_type
     {
-        NONE
+        PRIMITIVE, OPERATION, FUNCTION_CALL, DECLARATION
     };
 
     struct node
@@ -18,6 +20,18 @@ namespace
         node(node_type t)
         {
             type = t;
+        }
+    };
+
+    class abstract_syntax_tree
+    {
+    public:
+        node* root_node;
+
+        abstract_syntax_tree(std::vector<tokenizer::token>& tokens)
+        {
+            root_node = new node(PRIMITIVE);
+            
         }
     };
 }
